57b82dcdb89ce16e0a8b6743d59c2ad74118b8f1
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-01-22 22:14:52 -0500

    Update Main.java

--- SkyView/src/com/company/Main.java
+++ SkyView/src/com/company/Main.java
@@ -7,10 +7,9 @@ public class Main {
         double[]scan2={0.3,0.7,0.8,0.4,1.4,1.1};
         SkyView sv1 = new SkyView(4,3,scan);
         SkyView sv2=new SkyView(3,2,scan2);
-        System.out.println(sv1);
-        System.out.println(sv2);
+        System.out.println(sv1.sv());
+        System.out.println(sv2.sv());
         System.out.println("Average 1: "+sv1.getAverage(1,3,0,2));
         System.out.println("Average 2: "+sv2.getAverage(0,1,0,1));
-        // write your code here
     }
 }
